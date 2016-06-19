6f98ca6386618ed9de7ad1234c70f3869523ff28
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2016-06-18 22:44:50 -0300

    Write result to a file

--- src/github-contributors.cr
+++ src/github-contributors.cr
@@ -5,9 +5,17 @@ module Contributors
 
   extend Contributors::Response
 
+  def self.contributors(repository : String)
+    repo_contributors = Contributors.get_repo_contributors(repository)
+    issue_contributors = Contributors.get_issue_contributors(repository)
+    repo_contributors & issue_contributors
+  end
+
   def self.run
     options = Contributors::CLI.new
-    repo_contributors = Contributors.get_repo_contributors(options.repository)
-    issue_contributors = Contributors.get_issue_contributors(options.repository)
+    contributors = Contributors.contributors(options.repository)
+    File.open("contributors.txt", "w") do |f|
+      contributors.each{ |contributor| f.puts(contributor) }
+    end
   end
 end
